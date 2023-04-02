2f5a06dde4075736baa356b9bf6b7554c930a552
Saurabh SIngh <saurabhsinghup@gmail.com>
2023-04-03 02:18:25 +0530

    create element

--- script.js
+++ script.js
@@ -9,6 +9,18 @@ pt1[4].innerText="saurabh";
 pt1[4].style.backgroundColor="red";
 
     const listItems = document.querySelectorAll(".list-group-item");
-
-    // Change the font color of the second item to green
     listItems[1].style.color = "green";
+
+    // const xyz=document.getElementById("header-title");
+    // const helloWorld1 = document.createElement("li");
+    // x.querySelector(".header-title").innerHTML = "Hello World!";
+    // helloWorld1.textContent = "Hello word";
+    // xyz.before(helloWorld1);
+    var newDiv = document.createElement('div');
+    newDiv.className = 'hello';
+    newDiv.id ='hello1';
+    var newDivText =document.createTextNode('hello world');
+    newDiv.appendChild(newDivText);
+    var container = document.querySelector('header,container');
+    var h1=document.querySelector('header h1');
+    container.insertBefore(newDiv,h1);
