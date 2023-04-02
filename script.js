41488476c1e16db6a643ed2c3de219de73dbfc5a
Saurabh SIngh <saurabhsinghup@gmail.com>
2023-04-02 23:17:09 +0530

    queryselectorall

--- script.js
+++ script.js
@@ -2,8 +2,13 @@
 const pt=document.getElementsByClassName("list-group-item");
 console.log(pt[2]);
 pt[2].style.backgroundColor="green";
-// const pt=document.getElementsByClassName("normal");
-// pt.style.backgroundColor="red";
+const pt=document.getElementsByClassName("normal");
+pt.style.backgroundColor="red";
 const pt1=document.getElementsByTagName("li");
 pt1[4].innerText="saurabh";
 pt1[4].style.backgroundColor="red";
+
+    const listItems = document.querySelectorAll(".list-group-item");
+
+    // Change the font color of the second item to green
+    listItems[1].style.color = "green";
