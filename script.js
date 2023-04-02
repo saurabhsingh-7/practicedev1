63bbef13ba8040d22d8a51f2b24f5adca27345e7
Saurabh SIngh <saurabhsinghup@gmail.com>
2023-04-02 19:47:25 +0530

    using tagname edit

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 const pt=document.getElementsByClassName("list-group-item");
 console.log(pt[2]);
 pt[2].style.backgroundColor="green";
+// const pt=document.getElementsByClassName("normal");
+// pt.style.backgroundColor="red";
+const pt1=document.getElementsByTagName("li");
+pt1[4].innerText="saurabh";
+pt1[4].style.backgroundColor="red";
